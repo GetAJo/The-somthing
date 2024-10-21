6bede1c342e585faf3a5834a35d6f2f3216e5579
GetAJo <144846629+GetAJo@users.noreply.github.com>
2024-10-22 02:00:38 +0300

    Create Shared.h

---
+++ Project1/final_year_project/Shared.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <windows.h>
+#include <fstream> 
+#include <gdiplus.h>
+#include <gdiplusinit.h>
+
+#pragma once
+// somthing like a class to shere the data
+#ifndef SHARED_H
+#define SHARED_H
+//pointers
+POINT startPoint, endPoint;
+bool draged = false;
+std::vector<int> keysPressed;
+//the comindation of the keys pressed for the action keys are reccorded in ascii
+std::vector<int> keyCombo = { 160,162,164,191 };
+//make a file for the cords to be read from the other code
+std::ofstream file("selectedCordsOfTheUser.txt");
+
+#endif 
