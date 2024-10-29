0c72706f8d57ff0c92dd466a6cb5d0d56d0ed0f3
GetAJo <144846629+GetAJo@users.noreply.github.com>
2024-10-29 19:37:28 +0200

    Update Shared.h

--- Project1/final_year_project/Shared.h
+++ Project1/final_year_project/Shared.h
@@ -15,8 +15,10 @@ POINT startPoint, endPoint;
 bool draged = false;
 std::vector<int> keysPressed;
 //the comindation of the keys pressed for the action keys are reccorded in ascii
-std::vector<int> keyCombo = { 160,162,164,191 };
+std::vector<int> keyCombo = {160,162,164,191 };
 //make a file for the cords to be read from the other code
 std::ofstream file("selectedCordsOfTheUser.txt");
+//the juals
+std::string memTarhet;
 
 #endif 
